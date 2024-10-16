fbe2102a0e58346def6326615bc1b896541e7929
lovewln <731278138@qq.com>
2024-10-16 21:35:24 +0800

    Removed sensitive information

--- config.py
+++ config.py
@@ -5,19 +5,19 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 # Authentication for user filing issue (must have read/write access to repository to add issue to)
-USERNAME = ''
-TOKEN = ''
+USERNAME = os.getenv('GITHUB_USERNAME')
+TOKEN = os.getenv('GITHUB_TOKEN')
 
 # The repository to add this issue to
-REPO_OWNER = ''
-REPO_NAME = ''
+REPO_OWNER = 'lovewln'
+REPO_NAME = 'LlmDailyPapers'
 
 # Set new submission url of subject
 NEW_SUB_URL = 'https://arxiv.org/list/cs/new'
 
 # Keywords to search
-KEYWORD_LIST = ["", ]
+KEYWORD_LIST = ["large language model", "llm"]
 
 
-OPENAI_API_KEYS = ['', ]
+OPENAI_API_KEYS = [os.getenv('OPENAI_API_KEY')]
 LANGUAGE = "zh"  # zh | en
